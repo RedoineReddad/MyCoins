4f7beac4c89346ecd8b6e9b28babf1775cb71aed
RedoineReddad <reddad.redoine@gmail.com>
2020-12-29 13:11:28 +0100

    Added annotations for db's data class

--- app/src/main/java/com/example/mycoins/database/CoinData.kt
+++ app/src/main/java/com/example/mycoins/database/CoinData.kt
@@ -1,13 +1,28 @@
 package com.example.mycoins.database
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "coin_data_table")
 data class CoinData (
-    val id : String,
-    val symbol : String,
-    val name : String,
-    val image : String,
-    val current_price : Int,
-    val market_cap : Int,
-    val market_cap_rank : Int,
-    val price_change_percentage_24h : Int,
-    val last_updated : String
+        @PrimaryKey(autoGenerate = false)
+        val id : String,
+
+        @ColumnInfo(name = "symbol")
+        val symbol : String,
+        @ColumnInfo(name = "name")
+        val name : String,
+        @ColumnInfo(name = "image")
+        val image : String,
+        @ColumnInfo(name = "current_price")
+        val current_price : Int,
+        @ColumnInfo(name = "market_cap")
+        val market_cap : Int,
+        @ColumnInfo(name = "market_cap_rank")
+        val market_cap_rank : Int,
+        @ColumnInfo(name = "price_change_percentage_24h")
+        val price_change_percentage_24h : Int,
+        @ColumnInfo(name = "last_updated")
+        val last_updated : String
 )
